45f685fef2063db9cee0e83641fae8cb297b6866
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-12 06:01:41 +0900

    IsActiveを削除。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_111315) do
+ActiveRecord::Schema.define(version: 2019_07_11_205846) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "UserName"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2019_07_11_111315) do
 
   create_table "salaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "HourlySalary"
-    t.boolean "IsActive", default: true
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
