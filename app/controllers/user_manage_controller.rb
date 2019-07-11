7fad017c751d2f8a707153cc20a71eaa46635a3b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-12 06:53:07 +0900

    ユーザ追加処理を実装。

--- app/controllers/user_manage_controller.rb
+++ app/controllers/user_manage_controller.rb
@@ -1,4 +1,23 @@
 class UserManageController < ApplicationController
   def index
   end
+
+  def create
+    userName = params[:userName]
+    passWord = params[:passWord]
+    salaryId = params[:salaryId]
+    if userName.empty? || passWord.empty?
+      redirect_to "/user_manage", :alert => "ユーザ名かパスワードが入力されていません。"
+      return
+    end
+
+    @addUser = User.create(UserName: userName, PassWord: passWord, SalaryId: salaryId)
+    if !@addUser.save
+      redirect_to "/user_manage", :alert => "追加に失敗しました。"
+      return
+    end
+    
+    redirect_to "/user_manage", :notice => "追加しました。"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,11 @@ Rails.application.routes.draw do
   get 'admin_login' => 'admin_login#index'
   post 'admin_check' => 'admin_check#index'
   get 'admin_main' => 'admin_main#index'
-  get 'user_manage' => 'user_manage#index'
   get 'salary_manage' => 'salary_manage#index'
   post 'salary_manage' => 'salary_manage#create'
   post 'salary_manage/delete'
+  get 'user_manage' => 'user_manage#index'
+  post 'user_manage' => 'user_manage#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
