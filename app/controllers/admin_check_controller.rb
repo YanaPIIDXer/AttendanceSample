8e20070cdc366750463b3d32a6f1d9f3167301c5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-11 19:49:32 +0900

    認証処理実装。

--- app/controllers/admin_check_controller.rb
+++ app/controllers/admin_check_controller.rb
@@ -4,13 +4,26 @@ class AdminCheckController < ApplicationController
         userName = params[:session][:userName]
         passWord = params[:session][:passWord]
 
-        if(userName.empty? || passWord.empty?)
-            redirect_to "/admin_login", :alert => "ユーザ名かパスワードを入力してください。"
+        if userName.empty? || passWord.empty?
+            backToLoginForm("ユーザ名かパスワードを入力してください。")
             return
         end
 
+        if !Admin.isValid?(userName, passWord)
+            backToLoginForm("ユーザ名かパスワードが間違っています。")
+            return
+        end
+
+        session[:userName] = userName
+
         # 仮
         redirect_to "/admin_login", :alert => "ユーザ名：#{userName}"
+    end
+
+private
+
+    def backToLoginForm(alertMessage)
+        redirect_to "/admin_login", :alert => alertMessage
     end
 
 end
