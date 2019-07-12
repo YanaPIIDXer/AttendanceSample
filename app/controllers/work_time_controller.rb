3ae3e83a63b54a76dc5994604bcc2ee584838b83
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-12 10:00:33 +0900

    出勤処理を実装。

--- app/controllers/work_time_controller.rb
+++ app/controllers/work_time_controller.rb
@@ -1,6 +1,38 @@
 class WorkTimeController < ApplicationController
 
     def attendance
+        currentTime = DateTime.now
+        user = User.find(session[:userId])
+        if !user
+            redirect_to "/user_main", :alert => "出勤処理に失敗しました。"
+            return
+        end
+
+        if user.CurrentWorkTimeId != 0
+            redirect_to "/user_main", :alert => "既に出勤しています。"
+            return
+        end
+
+        salary = Salary.find(user.SalaryId)
+        if !salary
+            redirect_to "/user_main", :alert => "出勤処理に失敗しました。"
+            return
+        end
+
+        hourlySalary = salary.HourlySalary
+
+        workTime = WorkTime.create(StartTime: currentTime, HourlySalary: hourlySalary)
+        if !workTime.save
+            redirect_to "/user_main", :alert => "出勤処理に失敗しました。"
+            return
+        end
+
+        user.update(CurrentWorkTimeId: workTime.id)
+        if !user.save
+            redirect_to "/user_main", :alert => "出勤処理に失敗しました。"
+            return
+        end
+
         redirect_to "/user_main", :notice => "出勤しました。"
     end
 
